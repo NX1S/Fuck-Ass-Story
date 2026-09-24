90a80085ac5aaa5220f83e492a396511793fe079
Wael Al Hajj <NX1S@walhajj6@gmail.com>
2026-09-24 14:34:24 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,144 @@
+
+const firstaction = [
+    'Woke up with wet pants',
+    'Woke up to my alarm playing PH intro',
+    'Woke up to my dad\'s belt',
+    'Woke up to find out my gf is cheating right beside me on my bed',
+    'Woke up to shiz and cum',
+    'Woke up to my classmate having a baby on the bed with me',
+    'Woke up to waluwigi smashing me',
+    'woke up while my wife was having sex with our adopted son beside me',
+    'woke up listening to my daily dose of furry music',
+    'woke up thinking of obama\'s thicc ass',
+    'woke up naked with three naked dudes in my bed',
+]
+const thenaction = [
+    'went to check if its raining or not',
+    'went to turn on my pc',
+    'went to shit',
+    'went straight to the bar',
+    'cracked some cocaine',
+    'I put a condom on my dick',
+    'i farted but it turned out to be shit',
+    'went to feed my pet platypus',
+    'cried in the bathroom for 32.45338 minutes',
+    'prayed for lil uzi vert, may he rest in peace',
+    'poured the milk before the cereal',
+]
+const andaction = [
+    'ate a baby',
+    'watched porn',
+    'cummed',
+    'played MC',
+    'the homies called to join the party',
+    'had a sex with the dead body i found in the closet',
+    'wrote a hentai plot',
+    'said good morning to joe',
+    'wrote a 7683 word comment on reddit',
+    'told the chat to stop posting memes in general',
+    'said good morning to my minecraft gf',
+    'bombed a couple of towers in usa',
+]
+const whileaction = [
+    'checking my phone',
+    'DMing some bbs on discord',
+    'eating cereal',
+    'doing ma makeup',
+    'watching mukbang videos',
+    'doing a trickshot',
+    'ballancing a plate on a stick on my dick',
+    'doing my homeworks',
+    'getting spanked by my dad\'s belt',
+    'installing cyberpunk 2088 on my old computer',
+    'sexing my minecraft gf',
+    'sending nuclear weapons towards the sun',
+    'watching jojo',
+    'slapping bitches with my dick',
+]
+const suddenaction = [
+    'the dog shizzed and cummed on the ground',
+    'someone allahu akbar in my house',
+    'my RBG gaming mouse became gay',
+    'my gf became a succubus',
+    'my dick stopped working',
+    'pp shlong got bigger by 8 inches',
+    'i got pregnant with a cat',
+    'i am too fat',
+    'my huge penis stopped my movement',
+    'the minecraft enderdragon is waiting for me to beat him',
+    'found an rtx 3090ti on my doorstep',
+    'i got blood coming out of my dick',
+]
+const thoaction = [
+    'i was hungry',
+    'i was sad',
+    'i was horny',
+    'my cat died',
+    'i sharted',
+    'the sun exploded',
+    'my ass hurts',
+    'i got cheated on',
+    'wael\'s name is wael',
+    'i ruined the carpet with my cum',
+    'my minecraft gf left me',
+    'my gf was pregnant',
+]
+const Iaction = [
+    'continued on with my day',
+    'picked up my cat and threw it out from the window',
+    'listened to some lofi beat',
+    'made some tea, cus im bri\'ish',
+    'tried to join the Dream SMP',
+    'aborted the baby',
+    'me and the boys hanged out',
+    'became DickMaster69',
+    'still love you',
+    'raped a kid cus why not',
+    'teabagged on my brother in the 1v1',
+    'am groot',
+]
+const nowhereaction = [
+    'thanos snapped my ass',
+    'my penis got bigger ',
+    'robux scams are now legit',
+    'i got creative mode',
+    'i became a part of an anime',
+    'i became a part of a hentai',
+    'my laptop became an ipad',
+    'i became gay',
+    'i lost the penis privilege',
+    'i got a step-sis',
+    'my fbi came to my house',
+    'my waifu became real',
+    'i became a redditor',
+    'i became a discord mod',
+]
+const howeveraction = [
+    'power went out',
+    'you are still my habibi. :3',
+    'fuck me hard daddy uwu.',
+    'you never saw who really coded this bot...',
+    'you\'re gay. ',
+    'joe mama. 😔',
+    'the moon is going to collide with the earth within 2 weeks.',
+    'this is an illusion and all of this is fake...',
+    'you lost your virginity while sleeping.',
+    'mario mama\'d his last-a mia. 😔',
+    'a kind female stranger gave me a bj.',
+    'i got caught fapping to my gf\'s posts on ig.',
+    'my mom caught me dabbing on the toilet, not sure what to do next.',
+]
+
+
+const firstactions = firstaction[Math.floor(Math.random() * firstaction.length)];
+const secondactions = thenaction[Math.floor(Math.random() * thenaction.length)];
+const thirdactions = andaction[Math.floor(Math.random() * andaction.length)];
+const fourthactions = whileaction[Math.floor(Math.random() * whileaction.length)];
+const fifthactions = suddenaction[Math.floor(Math.random() * suddenaction.length)];
+const sixthaction = thoaction[Math.floor(Math.random() * thoaction.length)];
+const seventhaction = Iaction[Math.floor(Math.random() * Iaction.length)];
+const eigthaction = nowhereaction[Math.floor(Math.random() * nowhereaction.length)];
+const ninethaction = howeveraction[Math.floor(Math.random() * howeveraction.length)];
+
+console.log(`${firstactions} Then ${secondactions} and ${thirdactions} while ${fourthactions} suddenly ${fifthactions} tho ${sixthaction} I ${seventhaction} then out of nowhere ${eigthaction} however ${ninethaction}`)
+//story command
